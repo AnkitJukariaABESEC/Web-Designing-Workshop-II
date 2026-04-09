b71466aacd61940a6d2f311c80931c8e2a534c90
Ankit Jukaria <ankit.jukaria@gmail.com>
2026-04-09 12:20:49 +0530

    practical mt-1

---
+++ web-D mt-1 practical/index.jsx
@@ -0,0 +1,15 @@
+function display(){
+let character=document.getElementById("character").value;
+if(character=="a"||character=="e"||character=="i"||
+    character=="o"||character=="u"||character=="A"||character=="E"||character=="I"||
+    character=="O"||character=="U"){
+        output="The character is vowel";
+        document.getElementById("output").innerHTML=output;
+    }
+    else if{
+        output="The character is consonant";
+        document.getElementById("output").innerHTML=output;
+    }
+    
+}
+
